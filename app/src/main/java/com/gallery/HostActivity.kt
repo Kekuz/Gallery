ce270c07ec698079189431ff507e4dda40dd3202
Kekuz <anton.edlenko@yandex.ru>
2024-02-16 16:50:50 +0300

    Сделал сплеш

--- app/src/main/java/com/gallery/HostActivity.kt
+++ app/src/main/java/com/gallery/HostActivity.kt
@@ -2,9 +2,11 @@ package com.gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
     }
