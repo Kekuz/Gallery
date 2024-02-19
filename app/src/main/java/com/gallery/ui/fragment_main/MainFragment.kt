587b7058b8cbcfea1e8a6ca760dd1a22ab7eb68a
Kekuz <anton.edlenko@yandex.ru>
2024-02-19 16:49:13 +0300

    Кнопочки меняют цвет при нажатии, сделал табы и настроил их тему, сделал кастомный прогресс бар, сделал дизайн экрана домой

--- app/src/main/java/com/gallery/ui/fragment_main/MainFragment.kt
+++ app/src/main/java/com/gallery/ui/fragment_main/MainFragment.kt
@@ -6,15 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gallery.R
+import com.gallery.databinding.FragmentMainBinding
+import com.gallery.ui.fragment_main.pager.MainPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.pager.adapter = MainPagerAdapter(childFragmentManager, lifecycle)
+        binding.tabLayout.tabRippleColor = null
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            when(position) {
+                0 -> tab.text = getString(R.string.new_)
+                1 -> tab.text = getString(R.string.popular)
+            }
+        }.attach()
     }
 }
