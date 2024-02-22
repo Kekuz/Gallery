a49490e816d2a5477e53558e35d6d14dc8ded5ed
Kekuz <anton.edlenko@yandex.ru>
2024-02-22 23:11:18 +0300

    Небольшой хотфикс прокрутки интерфейса

--- app/src/main/java/com/gallery/ui/fragment_main/MainFragment.kt
+++ app/src/main/java/com/gallery/ui/fragment_main/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.gallery.R
 import com.gallery.databinding.FragmentMainBinding
 import com.gallery.ui.fragment_main.pager.MainPagerAdapter
@@ -13,6 +14,11 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,10 +35,15 @@ class MainFragment : Fragment() {
         binding.pager.adapter = MainPagerAdapter(childFragmentManager, lifecycle)
         binding.tabLayout.tabRippleColor = null
         TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
-            when(position) {
+            when (position) {
                 0 -> tab.text = getString(R.string.new_)
                 1 -> tab.text = getString(R.string.popular)
             }
         }.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
     }
 }
